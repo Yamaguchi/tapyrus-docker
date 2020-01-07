3c95dda98d15b920280ad322e05a514a25e1228d
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-01-07 22:53:04 +0900

    Work signer for v0.3.0

--- signer/run.sh
+++ signer/run.sh
@@ -4,13 +4,15 @@ trap 'kill $(jobs -p)' EXIT
 
 DELAY=5
 CARGO="cargo"
+LOG=/tmp/log/signer.log
+
+tail -v -n0 -F "$LOG" &
 
 export RUST_BACKTRACE=1
 while :
 do
 	$CARGO build --release
-  $CARGO install --force --path . 
-	./target/release/node $*
+  	$CARGO run --release -- $* 2>> "$LOG"
 	echo "Restarting in $DELAY seconds..."
 	sleep $DELAY
 done
