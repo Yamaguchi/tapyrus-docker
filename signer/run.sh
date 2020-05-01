ac49256faa3ec57583ba80c7564a2f24822a0891
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-05-01 15:32:28 +0900

    core-v0.4.0, signer-v0.4.0, and electrs-v0.2.0

--- signer/run.sh
+++ signer/run.sh
@@ -4,16 +4,16 @@ trap 'kill $(jobs -p)' EXIT
 
 DELAY=5
 CARGO="cargo"
-LOG=/tmp/log/signer.log
+LOG=/tmp/signer.log
 
 touch "$LOG"
 tail -v -n0 -F "$LOG" &
 
-export RUST_BACKTRACE=1
+export RUST_BACKTRACE=full
 while :
 do
 	$CARGO build --release
-  	$CARGO run --release -- $* 2>> "$LOG"
+  	$CARGO run --bin tapyrus-signerd --release -- $* 2>> "$LOG"
 	echo "Restarting in $DELAY seconds..."
 	sleep $DELAY
 done
